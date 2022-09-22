f4219018885bfb54111167fa5646d568cb8edfcd
Ilya Prushak <ilya.prushak@innowise-group.com>
2022-09-22 17:08:04 +0300

    GHIT-2: get access_token

--- app/src/Controller/GithubController.php
+++ app/src/Controller/GithubController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Services\GithubService;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GithubController extends AbstractController
 {
@@ -14,5 +16,23 @@ class GithubController extends AbstractController
             return $this->json('Total count of the term was not taken',Response::HTTP_BAD_REQUEST);
         }
         return $this->json(['totalCount'=>$githubService->score()],Response::HTTP_BAD_REQUEST);
+    }
+    
+    public function index(Request $request, HttpClientInterface $httpClient): Response
+    {
+    
+//
+//        $response = $httpClient->request('POST', 'https://github.com/login/oauth/access_token', [
+//            'headers' => [
+//                'content-type' => 'application/x-www-form-urlencoded',
+//            ],
+//            'body' => [
+//                'client_id' => 'adab2fd2c743280d0e81',
+//                'client_secret' => '96100d1251a49fe3323b0e73805b339c27b85d9b',
+//                'code' => $request->get('code'),
+//            ],
+//        ])->getContent();
+
+        return $this->json(1);
     }
 }
---
+++ front-end/src/components/Main.jsx
@@ -0,0 +1,9 @@
+import {GET_CODE} from "../vars/var";
+
+function Main() {
+    return(
+        <a href={GET_CODE}>GitHub log in</a>
+    )
+}
+
+export default Main;
